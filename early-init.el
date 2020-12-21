ff11f4c15346f32a0c666e07b7917558bae3c0a6
Adam Tjaden <algotastic@gmail.com>
2020-12-21 09:06:40 -0500

    Add base packages and modulate
    * Move customization to separate file
    * Move package-enable-at-startup to early-init to work with emacs 27
    * Disable startup message
    * Open init.el on load
    * Disable bars
    * Highlight active line
    * Show line numbers
    * Use use-package
    * Use spacemacs-theme
    * Use company-mode
    * Use use-package to enable evil-mode
    * Use magit
    * Use helm and set base keybindings
    * Use orgmode

---
+++ early-init.el
@@ -0,0 +1,2 @@
+;; Don't allow packages to be available until calling package-initialize (in init.el)
+(setq package-enable-at-startup nil)
--- init.el
+++ init.el
@@ -1,23 +1,80 @@
-(require 'package)
- 
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
- 
-(setq package-enable-at-startup nil)
-(package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Hide the tutorial page on startup
+(setq inhibit-startup-message t)
+(setq initial-buffer-choice "~/.emacs.d/init.el")
 
-(require 'evil)
-(evil-mode t)
+;; Disable tool bar, menu bar, scroll bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Highlight the active line
+(global-hl-line-mode t)
+;; Display line numbers
+(global-display-line-numbers-mode t)
+
+;; Use a different file for `custom-*` code
+(setq custom-file "~/.emacs.d/custom-file.el")
+;; Make sure that the code in custom-file is executed before the theme is loaded
+(load-file custom-file)
+
+;; Require and initialize `package`
+(require 'package)
+(package-initialize)
+ 
+;; Add `melpa` to `package-archives`
+;; Use t argument to append rather than prepend
+(add-to-list 'package-archives
+	     '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package spacemacs-common
+  :ensure spacemacs-theme
+  :init
+  ;; Don't use a different background color for comments
+  (setq spacemacs-theme-comment-bg nil)
+  ;; Comments appear in italics
+  (setq spacemacs-theme-comment-italic t)
+  :config
+  ;; Load the theme
+  (load-theme 'spacemacs-dark t))
+
+(use-package company
+  :ensure t
+  :init
+  ;; Make the company dropdown appear immediately
+  (setq company-idle-delay 0.0)
+  :config
+  ;; Enable company-mode everywhere
+  (global-company-mode t))
+
+(use-package evil
+  :ensure t
+  :config
+  ;; Enable evil-mode
+  (evil-mode t))
+
+(use-package magit
+  :ensure t)
+
+(use-package helm
+  :ensure t
+  :bind (("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files)
+	 ("C-x b" . helm-buffers-list)
+	 ("C-x c o" . helm-occur)
+	 ("M-y" . helm-show-kill-ring))
+  :config
+  (helm-mode 1))
+
+(use-package org
+  :ensure t)
